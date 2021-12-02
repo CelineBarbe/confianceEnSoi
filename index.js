6c4ceb5dd15cd840fe8d345297ce86b86a918d7c
CelineBarbe <laloutrecosmique@hotmail.fr>
2021-12-02 15:35:58 +0100

    deal with body input

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const axios = require('axios').default;
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const customQuotes = require('./quotes.json');
 
 app.use(router);
 
@@ -20,6 +23,15 @@ app.get('/', (request, response) => {
         console.log(error);
     });
 });
+
+app.post('/', jsonParser, (request, response) => {
+    let customQuote = request.body;
+    customQuotes.push(customQuote);
+    console.log(customQuotes);
+    response.status(201).json({
+    message: 'Objet créé !'
+    });
+})
 
 app.listen(port, () =>  {
     console.log('Cette app écoute le port : ' + port);
